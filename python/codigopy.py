154c2bad6582538c6afbda38ecb200d81abb5895
manuelamrodrigues <manuela.rodrigues@sptech.school>
2024-10-01 11:05:38 -0300

    Primeira tela de dashboard- Analista

--- python/codigopy.py
+++ python/codigopy.py
@@ -2,7 +2,15 @@ import psutil
 import time
 import subprocess
 from datetime import datetime
+from mysql.connector import connect, Error
 
+config = {
+  'user': 'root',
+  'password': 'manu',
+  'host': 'localhost',
+  'database': 'calculo',
+  'auth_plugin': 'mysql_native_password'
+}
 
 
 while (True):
@@ -31,6 +39,27 @@ while (True):
     print(f"pacotes recebidos: {rederecebimento}")
     print(f"tempo de boot: {boot}")
     print("------------------------------")
+    
+    try:
+        db = connect(**config)
+
+        
+        if db.is_connected():
+            db_info = db.get_server_info()
+            print('Connected to MySQL server version -', db_info)
+            
+            with db.cursor() as cursor:
+                query = ("INSERT INTO calculo.dados_capturados VALUES "
+                        f"(null, current_timestamp(), {cpu_percent}, {utilizado_ram / 1024/1024/1024}, {utilizado_armazenamento / 1024 / 1024 / 1024}, {redeenvio})")
+                cursor.execute(query)
+                db.commit()
+                print(cursor.rowcount, "registro inserido")
+            
+            cursor.close()
+            db.close()
+
+    except Error as e:
+        print('Error to connect with MySQL -', e) 
 
     time.sleep(10)
    
