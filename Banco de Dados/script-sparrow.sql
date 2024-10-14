fd84df3c705d4b4ce1fadc93b02532f1d2ca83a8
manuelamrodrigues <manuela.rodrigues@sptech.school>
2024-10-14 12:11:54 -0300

    Mudanças script banco de dados

--- Banco de Dados/script-sparrow.sql
+++ Banco de Dados/script-sparrow.sql
@@ -1,54 +1,47 @@
-create database Sparrow;
-use Sparrow;
-CREATE TABLE IF NOT EXISTS `empresa` (
+CREATE DATABASE IF NOT EXISTS `Sparrow`;
+USE `Sparrow` ;
+
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`empresa` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nome_empresa` VARCHAR(45) NULL,
-  `nome_representante` VARCHAR(45) NULL,
-  `email_contato` VARCHAR(255) NULL,
+  `nome_empresa` VARCHAR(255) NULL,
+  `nome_representante` VARCHAR(255) NULL,
+  `email_representante` VARCHAR(255) NULL,
   `cnpj` CHAR(14) NULL,
   PRIMARY KEY (`id`));
 
-INSERT INTO empresa VALUES (default, "Sparrow", "Dominique", "sparrow@gmail.com", 12345678912345);
 
-
-CREATE TABLE IF NOT EXISTS `cargo` (
-  `id` INT NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `Sparrow`.`cargo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-;
+  PRIMARY KEY (`id`));
 
-INSERT INTO cargo VALUES (default, "Administrador");
-INSERT INTO cargo VALUES (default, "Analista");
 
-CREATE TABLE IF NOT EXISTS `funcionario` (
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`funcionario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome_completo` VARCHAR(255) NULL,
   `email` VARCHAR(255) NULL,
   `senha` VARCHAR(45) NULL,
-  `empresa_id` INT NOT NULL,
-  `cargo_id` INT NOT NULL,
+  `fk_empresa` INT NOT NULL,
+  `fk_cargo` INT NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_funcionario_empresa1`
-    FOREIGN KEY (`empresa_id`)
+    FOREIGN KEY (`fk_empresa`)
     REFERENCES `Sparrow`.`empresa` (`id`),
   CONSTRAINT `fk_funcionario_cargo1`
-    FOREIGN KEY (`cargo_id`)
-    REFERENCES `Sparrow`.`cargo` (`id`)
-    );
+    FOREIGN KEY (`fk_cargo`)
+    REFERENCES `Sparrow`.`cargo` (`id`));
 
-INSERT INTO funcionario VALUES (default, "Dominique Falcone Dornan", "dominique.dornan@sptech.school", "sparrow", 1, 1);
 
-CREATE TABLE IF NOT EXISTS `tipo_maquina` (
-  `idtipo_maquina` INT NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `Sparrow`.`tipo_maquina` (
+  `idtipo_maquina` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
-  PRIMARY KEY (`idtipo_maquina`)
-  );
-
-INSERT INTO tipo_maquina VALUES (default, "Servidor");
-INSERT INTO tipo_maquina VALUES (default, "Terminal");
+  PRIMARY KEY (`idtipo_maquina`));
 
 
-CREATE TABLE IF NOT EXISTS `maquina` (
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`maquina` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `fk_empresa` INT NOT NULL,
   `fk_tipo_maquina` INT NOT NULL,
@@ -58,63 +51,95 @@ CREATE TABLE IF NOT EXISTS `maquina` (
     REFERENCES `Sparrow`.`empresa` (`id`),
   CONSTRAINT `fk_maquina_tipo_maquina1`
     FOREIGN KEY (`fk_tipo_maquina`)
-    REFERENCES `Sparrow`.`tipo_maquina` (`idtipo_maquina`)
-    );
+    REFERENCES `Sparrow`.`tipo_maquina` (`idtipo_maquina`));
 
 
-CREATE TABLE IF NOT EXISTS `tipo_componente` (
+CREATE TABLE IF NOT EXISTS `Sparrow`.`tipo_componente` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nome_componente` VARCHAR(45) NULL,
+  `nome_componente` VARCHAR(45),
   `unidade_medida` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-;
+  PRIMARY KEY (`id`));
+
+
+    CREATE TABLE IF NOT EXISTS `Sparrow`.`maquina_componente` (
+    `fk_maquina` INT NOT NULL,
+    `fk_componente_maquina` INT NOT NULL,
+    `limite_componente` INT NULL,
+    PRIMARY KEY (`fk_maquina`, `fk_componente_maquina`),
+    CONSTRAINT `fk_maquina_componente_maquina`
+        FOREIGN KEY (`fk_maquina`)
+        REFERENCES `Sparrow`.`maquina` (`id`),
+    CONSTRAINT `fk_maquina_componente_tipo_componente`
+        FOREIGN KEY (`fk_componente_maquina`)
+        REFERENCES `Sparrow`.`tipo_componente` (`id`)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`dado_capturado` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `registro` INT NULL,
+  `data_hora` DATETIME NULL,
+  `fk_maquina` INT NOT NULL,
+  `fk_componente_maquina` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_dados_servidor_servidor_componentes1`
+    FOREIGN KEY (`fk_maquina` , `fk_componente_maquina`)
+    REFERENCES `Sparrow`.`maquina_componente` (`fk_maquina` , `fk_componente_maquina`));
+
+
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`alerta` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fk_dado_maquina` INT NOT NULL,
+  PRIMARY KEY (`id`, `fk_dado_maquina`),
+  CONSTRAINT `fk_alertas_dados_servidor1`
+    FOREIGN KEY (`fk_dado_maquina`)
+    REFERENCES `Sparrow`.`dado_capturado` (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `Sparrow`.`lead` (
+  `id_lead` INT NOT NULL AUTO_INCREMENT,
+  `nome_empresa` VARCHAR(255) NULL,
+  `nome_representante` VARCHAR(255) NULL,
+  `email_representante` VARCHAR(255) NULL,
+  `cnpj` CHAR(14) NULL,
+  `descricao` VARCHAR(255) NULL,
+  PRIMARY KEY (`id_lead`));
+
+
+INSERT INTO empresa VALUES (default, "Sparrow", "Dominique", "sparrow@gmail.com", 12345678912345);
+
+INSERT INTO cargo VALUES (default, "Administrador");
+INSERT INTO cargo VALUES (default, "Analista");
+
+INSERT INTO funcionario VALUES (default, "Dominique Falcone Dornan", "dominique.dornan@sptech.school", "sparrow", 1, 1);
+
+INSERT INTO tipo_maquina VALUES (default, "Servidor");
+INSERT INTO tipo_maquina VALUES (default, "Terminal");
 
 INSERT INTO tipo_componente VALUES (default, "CPU", "Porcentagem");
 INSERT INTO tipo_componente VALUES (default, "RAM", "Porcentagem");
 INSERT INTO tipo_componente VALUES (default, "Disco", "Porcentagem");
-INSERT INTO tipo_componente VALUES (default, "Placa de Rede", "Inteiro");
+INSERT INTO tipo_componente VALUES (default, "Pacotes Enviados", "Inteiro");
+INSERT INTO tipo_componente VALUES (default, "Pacotes Recebidos", "Inteiro");
+SELECT * FROM tipo_componente;
+
+INSERT INTO maquina VALUES (default, 1, 1);
+INSERT INTO maquina_componente VALUES (1, 4, 10000);
+INSERT INTO maquina_componente VALUES (1, 5, 10000);
+
+SELECT * FROM maquina_componente;
+SELECT * FROM dado_capturado;
 
 
-CREATE TABLE IF NOT EXISTS `maquina_componente` (
-  `fk_servidor` INT NOT NULL auto_increment,
-  `fk_componente_servidor` INT NOT NULL,
-  `limite_componente` INT NULL,
-  PRIMARY KEY (`fk_servidor`, `fk_componente_servidor`),
-  CONSTRAINT `fk_componentes_servidor_has_servidor_componentes_servidor1`
-    FOREIGN KEY (`fk_servidor`)
-    REFERENCES `Sparrow`.`maquina` (`id`),
-  CONSTRAINT `fk_componentes_servidor_has_servidor_servidor1`
-    FOREIGN KEY (`fk_componente_servidor`)
-    REFERENCES `Sparrow`.`tipo_componente` (`id`)
-    );
 
-CREATE TABLE IF NOT EXISTS `dado_capturado` (
-  `id` INT NOT NULL auto_increment,
-  `registro` INT NULL,
-  `data_hora` DATETIME NULL,
-  `fk_servidor` INT NOT NULL,
-  `fk_componente_servidor` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_dados_servidor_servidor_componentes1`
-    FOREIGN KEY (`fk_servidor` , `fk_componente_servidor`)
-    REFERENCES `Sparrow`.`maquina_componente` (`fk_servidor` , `fk_componente_servidor`)
-    );
 
-CREATE TABLE IF NOT EXISTS `alerta` (
-  `id` INT NOT NULL auto_increment,
-  `fk_dado_servidor` INT NOT NULL,
-  PRIMARY KEY (`id`, `fk_dado_servidor`),
-  CONSTRAINT `fk_alertas_dados_servidor1`
-    FOREIGN KEY (`fk_dado_servidor`)
-    REFERENCES `Sparrow`.`dado_capturado` (`id`)
-    );
 
-CREATE TABLE IF NOT EXISTS `contato` (
-  `id_contatos` INT NOT NULL auto_increment,
-  `nome_empresa` VARCHAR(45) NULL,
-  `nome_representante` VARCHAR(45) NULL,
-  `email_contato` VARCHAR(45) NULL,
-  `cnpj` CHAR(14) NULL,
-  `descricao` VARCHAR(255),
-  PRIMARY KEY (`id_contatos`)
-  );
+
+
+
+
+
+
+
