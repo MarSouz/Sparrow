cb48e08d7502a52d85e55c32a3f9c9a7dc4f7aa4
elersonleandro <elerson.alves@sptech.school>
2024-10-15 16:57:06 -0300

    Criando o jar executável do projeto

--- Looca/src/main/kotlin/Main.kt
+++ Looca/src/main/kotlin/Main.kt
@@ -1,14 +1,11 @@
 import com.github.britooo.looca.api.core.Looca
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.system.exitProcess
 
 open class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             println("Olá! Seja bem-vindo à Sparrow Tech Solutions. Estamos aqui para ajudá-lo a capturar os pacotes de rede e monitorar sua conexão.")
-            print("Pronto para começar? Digite 1 para iniciar a captura: ")
+            print("Pronto para começar? Digite 1 para iniciar: ")
 
             val inicio = readln().toInt()
 
@@ -16,57 +13,60 @@ open class Main {
                 val repositorio = MaquinaRepositorio()
                 repositorio.configurar()
                 val looca = Looca()
+                var interfaces = looca.rede.grupoDeInterfaces.interfaces
+                var interfaceDeConexaoPrincipal = interfaces.filter { it.nome.lowercase().contains("wlan2") }
+                val existeMac = repositorio.existePorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
+                if (existeMac) {
+                    println("Máquina já cadastrada. Iniciando captura...")
+                } else {
+                    println("Cadastrando máquina no sistema. Iniciando captura...")
+                    val novaMaquina = Maquina()
+                    novaMaquina.setEnderecoMac(interfaceDeConexaoPrincipal[0].enderecoMac)
+                    repositorio.cadastrarMaquina(novaMaquina)
+
+                    val maquinaCadastrada = repositorio.buscarPorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
+
+                    repositorio.cadastrarComponente(maquinaCadastrada.id, 4)
+                    repositorio.cadastrarComponente(maquinaCadastrada.id, 5)
+
+                }
 
                 while (true) {
+                    interfaces = looca.rede.grupoDeInterfaces.interfaces
 
-                    val interfaces = looca.rede.grupoDeInterfaces.interfaces
+                    interfaceDeConexaoPrincipal = interfaces.filter { it.nome.lowercase().contains("wlan2") }
 
-                    val interfaceDeConexaoPrincipal = interfaces.filter { it.nome.lowercase().contains("wlan2") }
-                    val existeMac = repositorio.existePorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
+                    if (interfaceDeConexaoPrincipal.isNotEmpty()) {
+                        val pacotesEnviados = interfaceDeConexaoPrincipal[0].pacotesEnviados
+                        val pacotesRecebidos = interfaceDeConexaoPrincipal[0].pacotesRecebidos
+                        val maquina = repositorio.buscarPorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
+                        val dadoPacotesEnviados = DadoCapturado().apply {
+                            setFkMaquina(maquina.id)
+                            setRegistro(pacotesEnviados)
+                            setComponenteMaquina(4)
+                        }
+                        val sucessoEnviados = repositorio.inserir(dadoPacotesEnviados)
 
-                    if (existeMac){
-                        if (interfaceDeConexaoPrincipal.isNotEmpty()) {
-                            val pacotesEnviados = interfaceDeConexaoPrincipal[0].pacotesEnviados
-                            val pacotesRecebidos = interfaceDeConexaoPrincipal[0].pacotesRecebidos
-                            val maquina = repositorio.buscarPorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
-                            val dadoPacotesEnviados = DadoCapturado().apply {
-                                setFkMaquina(maquina.id)
-                                setRegistro(pacotesEnviados)
-                                setComponenteMaquina(4)
-                            }
-                            val sucessoEnviados = repositorio.inserir(dadoPacotesEnviados)
+                        val dadoPacotesRecebidos = DadoCapturado().apply {
+                            setFkMaquina(maquina.id)
+                            setRegistro(pacotesRecebidos)
+                            setComponenteMaquina(5)
+                        }
+                        val sucessoRecebidos = repositorio.inserir(dadoPacotesRecebidos)
 
-                            val dadoPacotesRecebidos = DadoCapturado().apply {
-                                setFkMaquina(maquina.id)
-                                setRegistro(pacotesRecebidos)
-                                setComponenteMaquina(5)
-                            }
-                            val sucessoRecebidos = repositorio.inserir(dadoPacotesRecebidos)
-
-                            println(
-                                """
+                        println(
+                            """
                                 Pacotes Enviados: $pacotesEnviados
                                 Pacotes Recebidos: $pacotesRecebidos
                                 """.trimIndent()
-                            )
+                        )
 
-                            if (sucessoEnviados && sucessoRecebidos) {
-                                println("Inserido no banco de dados com sucesso!")
-                            }
-                            Thread.sleep(10000)
+                        if (sucessoEnviados && sucessoRecebidos) {
+                            println("Inserido no banco de dados com sucesso!")
                         }
+                        Thread.sleep(10000)
                     }
-                    else{
-                        val novaMaquina = Maquina()
-                        novaMaquina.setEnderecoMac(interfaceDeConexaoPrincipal[0].enderecoMac)
-                        repositorio.cadastrarMaquina(novaMaquina)
 
-                        val maquinaCadastrada = repositorio.buscarPorMac(interfaceDeConexaoPrincipal[0].enderecoMac)
-
-                        repositorio.cadastrarComponente(maquinaCadastrada.id, 4)
-                        repositorio.cadastrarComponente(maquinaCadastrada.id, 5)
-
-                    }
                 }
             } else {
                 println("Opção inválida.")
